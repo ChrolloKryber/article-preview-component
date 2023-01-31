ef9691ac5ac89ebdb80d59b250a9378d74ed8239
TruffleWorm <rishay.raghav00@gmail.com>
2023-01-31 15:52:38 +0530

    added some fade animations

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ $(document).ready(function () {
     $('.footer').hide();
     $('.share').click(function (e) { 
         e.preventDefault();
-        $('.footer').toggle();
+        $('.footer').fadeToggle(500);
         $('.share').toggleClass('button-pressed');
     });
 });
+
